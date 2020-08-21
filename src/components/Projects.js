cc69ad0b194944b25927bf03e5276588ae484110
Adrian Rosales <adrian.e.rosales@gmail.com>
2020-08-21 15:16:45 -0700

    need to rest brain for a second

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { base64ToImage } from '../helperMethods/base64ToImage'
+var _ = require('lodash')
 
 
 class Projects extends React.Component {
@@ -62,129 +63,56 @@ class Projects extends React.Component {
     )
   }
 
-  establishTechIconsTwo = (item, index) => {
-    console.log('establishTechIconsTwo running...')
-    console.log(item, index)
-    let languages = [] 
-    let frontEndFWS = []
-    let backEndFWS = []
-    let libraries = [] 
-    let testingLibraries = [] 
-    let uiFWS = [] 
-    let CMS = [] 
-    const technologies = []
-    let techTypeAreas = []
-    let techTypeIds = []
-    const stateTech = this.state.techData || []
-    let techMarkup = []
-    //Compare the item.technology array elements against the tech_ids in the state
+  getTechGroups = (item) => {
+    let stateTech = this.state.techData || []
+    let technologiesUsed = []
+
     item.technologies.forEach(number => {
       stateTech.forEach(item => {
         if(number == item.tech_id) {
-          technologies.push(item)
+          technologiesUsed.push(item)
         }
       })
     })
-    console.log(technologies)
-    technologies.forEach(item => {
 
-      techTypeIds.push(item.technology_type_id)
-      if(item.technology_type_id == 1) {
-        languages.push(item)
-      }
-      if(item.technology_type_id == 2) {
-        frontEndFWS.push(item)
-      }
-      if(item.technology_type_id == 3) {
-        backEndFWS.push(item)
-      }
-      if(item.technology_type_id == 4) {
-        libraries.push(item)
-      }
-      if(item.technology_type_id == 5) {
-        testingLibraries.push(item)
-      }
-      if(item.technology_type_id == 6) {
-        uiFWS.push(item)
-      }
-      if(item.technology_type_id == 7) {
-        CMS.push(item)
-      }
-    })
-    console.log(languages, frontEndFWS, backEndFWS, libraries, testingLibraries, uiFWS, CMS)
-    //establish array of the type-zones we will create "Front End" "Language" "Library" we create based off of the matches
-    techTypeAreas = new Set(techTypeIds)
-    console.log(techTypeAreas)
-    techTypeAreas.forEach(number => {
-      let markupPiece = ''
-      switch(number) {
-        case 1:
-          markupPiece = `
-            <div className="languageArea">
-              <h3>language</h3>
-            </div>
-          `
-          techMarkup.push(markupPiece)
-          break
-        case 2: 
-          markupPiece = `
-            <div>
-              <h3>front-end framework</h3>
-            </div>
-          `
-          techMarkup.push(markupPiece)
-          break
-        case 3: 
-          markupPiece = `
-            <div>
-              <h3>back-end framework</h3>
-            </div>
-          `
-          techMarkup.push(markupPiece)
-          break
-        case 4: 
-          markupPiece = `
-            <div>
-              <h3>library</h3>
-            </div>
-          `
-          techMarkup.push(markupPiece)
-          break
-        case 5: 
-          markupPiece = `
-            <div>
-              <h3>testing library</h3>
-            </div>
-          `
-          techMarkup.push(markupPiece)
-          break
-        case 6: 
-          markupPiece = `
-            <div>
-              <h3>ui framework</h3>
-            </div>
-          `
-          techMarkup.push(markupPiece)
-          break
-        case 7: 
-          markupPiece = `
-              <div>
-                <h3>content management system</h3>
-              </div>
-            `
-            techMarkup.push(markupPiece)
-            break
-        default: 
-          console.log('w/e')
-      }
-    })
-    console.log('PAISHO', techMarkup)
-    //return type-zones that show the tech-type, icons as clickable entries for the tech, tech name
+    technologiesUsed = technologiesUsed.sort((a,b) => (a.technology_type_id > b.technology_type_id) ? 1 : -1)
+    console.log(technologiesUsed)
+
+
+    console.log(  _.groupBy(technologiesUsed, 'technology_type_id'))
+    let techGroups = _.groupBy(technologiesUsed, 'technology_type_id'.toString())
+    return techGroups
   }
+
+
 
   render() {
     console.log('projects rendering', this.state)
     const projects = (this.state.data).map((item, index) => {
+      let techGroups = this.getTechGroups(item)
+      let languages = techGroups[1] || []
+      let frontEndFWS = techGroups[2]
+      let backEndFWS = techGroups[3]
+      let libraries = techGroups[4]
+      let testingLibraries = techGroups[5]
+      let uiFWS = techGroups[6]
+      let cms = techGroups[7]
+      console.log(languages)
+      
+      const languageTech = languages.map((item,index)=> {
+        let langImage = base64ToImage(item)
+        return (
+          <div key={index}>
+            <a href={item.tech_website}>
+              <img src={langImage} />
+              {item.name}
+            </a>
+          </div>
+        )
+      })
+
+      
+
       const imageSrc = base64ToImage(item)
       return (
         <div key={index}>
@@ -198,8 +126,7 @@ class Projects extends React.Component {
             <p>{item.summary}</p>
               <div>
                 <h5>Technologies:</h5>
-                  {this.establishTechIconsTwo(item, index)}
-                {/* {this.establishTechIcons(JSON.parse(item.technologies))} */}
+                {languageTech}
               </div>
               <div>
                 <h5>Links:</h5>
---
+++ src/helperMethods/makeTechSection.js
@@ -0,0 +1,3 @@
+const makeTechSection = (sectionNameID, data) => {
+  
+}
